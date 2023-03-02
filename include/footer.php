9c423df7942e9a36de60d2675521def89d5b26e6
frainerCalia <frainer.simarra@calia.ai>
2023-03-02 13:38:32 -0500

    UPDATE FOOTER

--- include/footer.php
+++ include/footer.php
@@ -150,13 +150,13 @@
                         </div>
                         <div class="book_popup_model_text">
                             <h3>FIND YOUR NEAREST LOCATION</h3>
-                            <p class="lead_txt">Find your local Insulationusa Insulation by typing in your zip code.</p>
+                            <p class="lead_txt">call us and communicate with us</p>
                             <form action="https://koalainsulation.com/find_zip_code" class="find_nearest_form  mt-3">
                                 <input type="hidden" name="_token" value="y9IhyvSlBIQvMY2spG6BKiqzTAfoOrz3FRoQaku4">
                                 <input type="text" name="zip_code" maxlength="6" class="form-control"
                                     placeholder="Enter Your ZIP Code">
-                                <button type="button" class="custom_btn_orange find_nearest_btn_popup fynl_btn">FIND
-                                    YOUR NEAREST LOCATION</button>
+                                    <a href="tel://9543036361">
+                                <button type="button" class="custom_btn_orange find_nearest_btn_popup fynl_btn">CALL NOW</button></a>
                             </form>
                         </div>
                     </div>
